11674ba21179771771abf29e75d0e8527f4df7a6
JeanP2501 <jeanydrogos@gmail.com>
2025-12-02 16:01:59 -0500

    feat: eureka client impementation

--- src/main/java/com/bank/account/AccountApplication.java
+++ src/main/java/com/bank/account/AccountApplication.java
@@ -2,12 +2,14 @@ package com.bank.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Main application class for Account Service
  * Manages bank accounts (passive products)
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AccountApplication {
 
 	public static void main(String[] args) {
